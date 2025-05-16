4c0f16ebd957810e38ad42c885c01c481b55315b
Dinesh chandra <dineshchandrakonala@gmail.com>
2025-05-16 15:10:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import NavbarMain from './components/navbar/NavbarMain'
-import HeroMain from './components/heroSection/heroMain';
-import HeroGradient from './components/heroSection/heroGradient';
+import HeroMain from './components/heroSection/HeroMain';
+import HeroGradient from './components/heroSection/HeroGradient';
 import SubHeroSection from './components/heroSection/SubHeroSection';
 import AboutMeMain from './components/aboutMeSection/AboutMeMain';
 import SkillsMain from './components/skillSection/SkillsMain';
 import SubSkills from './components/skillSection/SubSkills';
 import ExperienceMain from './components/experienceSection/ExperienceMain';
-import ProjectMain from './components/projectSection/projectMain';
+import ProjectMain from './components/projectSection/ProjectMain';
 import ContactMeMain from "./components/contactMeSection/ContactMeMain";
 import FooterSection from "./components/footerSection/FooterMain"
 function App() {
