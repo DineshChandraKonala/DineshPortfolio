a4ce336bdbaaf11858fa61a1759097a6302558a8
Dinesh chandra <dineshchandrakonala@gmail.com>
2025-07-12 12:22:17 +0530

    Update AboutMeText.jsx

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -8,7 +8,7 @@ export default function AboutMeText(){
                 database optimization, and problem-solving (DSA). 
                 Passionate about performance optimization and scalable web
                 applications.</p>
-               <a href="https://drive.google.com/file/d/14OXBvoQPpNlKYX0iXzcbaMPSrN2WXGGK/view?usp=drive_link">
+               <a href="https://drive.google.com/file/d/1-bLhDfGOUZzJ7uQhyMC9d6VMrRiZIrVS/view?usp=sharing">
                     <button className="border border-orange rounded-full py-2 px-4 text-lg flex items-center mt-10
                      hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center
                       text-white hover:text-cyan">MyResume</button></a>
